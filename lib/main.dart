5093b61ba3ca528955782b5e4d86e1f2d43dd780
asousapt <t.meditas@gmail.com>
2024-04-24 09:34:52 +0100

    ecra perfil

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:softshares_mobile/screens/home.dart';
-import 'package:softshares_mobile/screens/perfil.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: HomeScreen(),
-      routes: {
-        '/perfil': (context) => const ProfileScreen(),
-      },
     );
   }
 }
