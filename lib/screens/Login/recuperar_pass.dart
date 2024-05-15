96b840ca6364c7428dd92684ac9250b4817b2ed7
asousapt <t.meditas@gmail.com>
2024-05-15 21:51:02 +0100

    Fix do ecra de recuperar password e login

--- lib/screens/Login/recuperar_pass.dart
+++ lib/screens/Login/recuperar_pass.dart
@@ -70,93 +70,94 @@ class _EcraRecPassState extends State<EcraRecPass> {
       body: Padding(
         padding: const EdgeInsets.only(left: 12, right: 12, top: 90),
         child: SingleChildScrollView(
-          child: Container(
-            height: (MediaQuery.of(context).size.height) * 1 -
-                90, // Adjust height as needed
-            child: Column(
-              children: [
-                Container(
-                  height: (MediaQuery.of(context).size.height) * 0.8,
+          child: Column(
+            children: [
+              Container(
+                height: (MediaQuery.of(context).size.height) * 0.8,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
                   color: Theme.of(context).canvasColor,
-                  child: Column(
-                    children: [
-                      const SizedBox(height: 30),
-                      Text(
-                        AppLocalizations.of(context)!.recuperarPass,
-                        style: TextStyle(
-                          fontSize: 36,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      SizedBox(
-                          height: (MediaQuery.of(context).size.height) * 0.20),
-                      Container(
-                        margin: const EdgeInsets.only(
-                            left: 8, right: 8, bottom: 30),
-                        child: Form(
-                          key: _formKey,
-                          child: Column(
-                            children: [
-                              TextFormField(
-                                keyboardType: TextInputType.emailAddress,
-                                controller: controlEmail,
-                                decoration: const InputDecoration(
-                                  labelText: "Email",
-                                  prefixIcon:
-                                      Icon(Icons.account_circle_outlined),
-                                ),
-                                validator: (value) {
-                                  if (value == null ||
-                                      value.isEmpty ||
-                                      !isValidEmail(value)) {
-                                    return AppLocalizations.of(context)!
-                                        .insiraEmaiValido;
-                                  }
-                                  return null;
-                                },
-                              ),
-                              Container(
-                                padding: EdgeInsets.fromLTRB(0, 30, 0, 10),
-                                child: SizedBox(
-                                  height: 70,
-                                  width: double.infinity,
-                                  child: FilledButton(
-                                    onPressed: () {
-                                      Navigator.pushNamed(context, '/confirmarID');
-                                    },
-                                    child: Text(AppLocalizations.of(context)!
-                                        .continuar),
-                                  ),
-                                ),
-                              ),
-                              SizedBox(
-                                  height: (MediaQuery.of(context).size.height) *
-                                      0.1),
-                              Logo()
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
                 ),
-                SizedBox(height: (MediaQuery.of(context).size.height) * 0.05),
-                Row(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    const Icon(
-                      Icons.copyright,
-                      color: Color.fromRGBO(230, 230, 230, 1),
-                    ),
-                    const SizedBox(width: 3),
+                    const SizedBox(height: 30),
                     Text(
-                      "Softinsa v$version",
+                      textAlign: TextAlign.center,
+                      AppLocalizations.of(context)!.recuperarPass,
                       style: const TextStyle(
-                          color: Color.fromRGBO(230, 230, 230, 1)),
-                    )
+                        fontSize: 36,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(
+                        height: (MediaQuery.of(context).size.height) * 0.10),
+                    Container(
+                      margin:
+                          const EdgeInsets.only(left: 8, right: 8, bottom: 30),
+                      child: Form(
+                        key: _formKey,
+                        child: Column(
+                          children: [
+                            TextFormField(
+                              keyboardType: TextInputType.emailAddress,
+                              controller: controlEmail,
+                              decoration: const InputDecoration(
+                                labelText: "Email",
+                                prefixIcon: Icon(Icons.account_circle_outlined),
+                              ),
+                              validator: (value) {
+                                if (value == null ||
+                                    value.isEmpty ||
+                                    !isValidEmail(value)) {
+                                  return AppLocalizations.of(context)!
+                                      .insiraEmaiValido;
+                                }
+                                return null;
+                              },
+                            ),
+                            Container(
+                              padding: const EdgeInsets.fromLTRB(0, 30, 0, 10),
+                              child: SizedBox(
+                                height: 50,
+                                width: double.infinity,
+                                child: FilledButton(
+                                  onPressed: () {
+                                    Navigator.pushNamed(
+                                        context, '/confirmarID');
+                                  },
+                                  child: Text(
+                                      AppLocalizations.of(context)!.continuar),
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                                height:
+                                    (MediaQuery.of(context).size.height) * 0.1),
+                            Logo()
+                          ],
+                        ),
+                      ),
+                    ),
                   ],
-                )
-              ],
-            ),
+                ),
+              ),
+              SizedBox(height: (MediaQuery.of(context).size.height) * 0.05),
+              Row(
+                children: [
+                  const Icon(
+                    Icons.copyright,
+                    color: Color.fromRGBO(230, 230, 230, 1),
+                  ),
+                  const SizedBox(width: 3),
+                  Text(
+                    "Softinsa v$version",
+                    style: const TextStyle(
+                        color: Color.fromRGBO(230, 230, 230, 1)),
+                  )
+                ],
+              )
+            ],
           ),
         ),
       ),
