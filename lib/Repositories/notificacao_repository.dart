8290bc159374351298821b7a60902bab43d823da
asousapt <t.meditas@gmail.com>
2024-07-11 16:17:57 +0100

    Filtro de notificações a funcionar

--- lib/Repositories/notificacao_repository.dart
+++ lib/Repositories/notificacao_repository.dart
@@ -12,16 +12,11 @@ class NotificacaoRepository {
   Future<List<Notificacao>> getNotificacoes() async {
     List<Notificacao> listaFinal = [];
     List<Notificacao> listaNotif = [];
-    NotificationPreferenceRepository _notificationPreferenceRepository =
+    NotificationPreferenceRepository notificationPreferenceRepository =
         NotificationPreferenceRepository();
 
-    //List<NotificationPreference> listaPref =
-    // await _notificationPreferenceRepository
-    //   .fetchLocalNotificationPreferences();
-    /*for (var preferencias in listaPref) {
-      print(preferencias.type);
-      print(preferencias.enabled);
-    }*/
+    List<NotificationPreference> listaPref =
+        await notificationPreferenceRepository.getPrefsUtil();
 
     final prefs = await SharedPreferences.getInstance();
     String util = prefs.getString("utilizadorObj") ?? "";
@@ -39,6 +34,17 @@ class NotificacaoRepository {
           .toList();
     }
 
+    // filtra o tipo de notificação que o utilizador quer receber
+    for (var notif in listaNotif) {
+      for (var pref in listaPref) {
+        if (notif.tipo == pref.type) {
+          if (pref.enabled) {
+            listaFinal.add(notif);
+          }
+        }
+      }
+    }
+
     return listaFinal;
   }
 
@@ -53,5 +59,44 @@ class NotificacaoRepository {
     } else {
       return false;
     }
+  }
+
+  Future<int> getNmrNotificacoes() async {
+    List<Notificacao> listaNotif = [];
+    NotificationPreferenceRepository notificationPreferenceRepository =
+        NotificationPreferenceRepository();
+    int numeroNotificacoes = 0;
+
+    List<NotificationPreference> listaPref =
+        await notificationPreferenceRepository.getPrefsUtil();
+
+    final prefs = await SharedPreferences.getInstance();
+    String util = prefs.getString("utilizadorObj") ?? "";
+    Utilizador utilizador = Utilizador.fromJson(jsonDecode(util));
+    int utilizadorId = utilizador.utilizadorId;
+    _apiService.setAuthToken("tokenFixo");
+
+    final url = "notificacao/utilizador/$utilizadorId/naolidas";
+    final response = await _apiService.getRequest(url);
+    final notificacoesFormatted = response['data'];
+
+    if (notificacoesFormatted != null) {
+      listaNotif = (notificacoesFormatted as List)
+          .map((item) => Notificacao.fromJson(item))
+          .toList();
+    }
+
+    // filtra o tipo de notificação que o utilizador quer receber
+    for (var notif in listaNotif) {
+      for (var pref in listaPref) {
+        if (notif.tipo == pref.type) {
+          if (pref.enabled) {
+            numeroNotificacoes++;
+          }
+        }
+      }
+    }
+
+    return numeroNotificacoes;
   }
 }
